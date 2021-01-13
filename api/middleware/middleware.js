8f6896f56675a01836895bdd31af7d40917f869c
ruizaj13 <ruizaj13@gmail.com>
2021-01-13 13:15:24 -0800

    logger is complete and functioning

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,7 @@
 function logger(req, res, next) {
-  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`)
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`,
+  next()
+  )
 }
 
 function validateUserId(req, res, next) {
--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
 const server = express();
+const {logger} = require('./middleware/middleware')
 
 server.use(express.json())
 
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and routes need to be connected here
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
